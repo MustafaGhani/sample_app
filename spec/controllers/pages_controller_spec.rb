92c81368919bb9ac4ae372228ead48976e9a2d37
ReneDogs <shingen.takeda@gmx.net>
2011-05-02 19:24:48 +0200

    Done with exercise 3.5 and finish Tests

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -59,7 +59,8 @@ end
     it "should have the right title" do
       get 'help'
       response.should have_selector("title",
-                        :content => @base_title + "| Help")
+                        :content =>
+                          @base_title + " | Help")
     end
   end
 end
